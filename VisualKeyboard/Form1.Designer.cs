a3e00171097a190827b325238c6581884c0ea437
Teijo Laine <teijo@aropupu.fi>
2015-07-17 13:45:41 +0300

    Pressing A with or without focus flashes box color

--- VisualKeyboard/Form1.Designer.cs
+++ VisualKeyboard/Form1.Designer.cs
@@ -17,23 +17,37 @@
 
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Yellow;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 20);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- VisualKeyboard/Form1.cs
+++ VisualKeyboard/Form1.cs
@@ -14,9 +14,16 @@ namespace VisualKeyboard
         [DllImportAttribute("user32.dll")]
         public static extern bool ReleaseCapture();
 
+        const int KEY_ID = 1;
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
         public Form1()
         {
             InitializeComponent();
+            // Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+            RegisterHotKey(this.Handle, KEY_ID, 0, (int)Keys.A);
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
@@ -26,6 +33,27 @@ namespace VisualKeyboard
                 ReleaseCapture();
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == KEY_ID)
+            {
+                textBox1.BackColor = System.Drawing.Color.Red;
+                var timer = new Timer();
+                timer.Tick += (s, e) => {
+                    ((Timer)s).Stop();
+                    textBox1.BackColor = System.Drawing.Color.Yellow;
+                };
+                timer.Interval = 1000;
+                timer.Start();
+            }
+            base.WndProc(ref m);
         }
     }
 }

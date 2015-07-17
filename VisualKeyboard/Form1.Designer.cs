d9a019f9efffcf1605d362310473a7bf4a963a89
Teijo Laine <teijo@aropupu.fi>
2015-07-17 23:24:17 +0300

    AutoSize window

--- VisualKeyboard/Form1.Designer.cs
+++ VisualKeyboard/Form1.Designer.cs
@@ -73,15 +73,14 @@ namespace VisualKeyboard
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.AutoSize = true;
             this.ResumeLayout(false);
             this.PerformLayout();
         }

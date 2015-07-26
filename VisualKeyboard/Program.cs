35ca20565c0787dd286b9a3c44195020507c014b
Teijo Laine <teijo@aropupu.fi>
2015-07-26 20:59:34 +0300

    Relocate InputKey to Program.cs

--- VisualKeyboard/Program.cs
+++ VisualKeyboard/Program.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using VisualKeyboard.Properties;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using System.Reactive;
 
 namespace VisualKeyboard
 {
@@ -75,6 +78,51 @@ namespace VisualKeyboard
             };
         }
 
+    }
+
+    public class InputKey : TextBox
+    {
+        public readonly Keys Key;
+        private readonly IDisposable Unsubscribe;
+        private event EventHandler KeyEvent;
+
+        public InputKey(Keys keyCode)
+        {
+            Key = keyCode;
+            BorderStyle = BorderStyle.None;
+            Enabled = false;
+            Dock = DockStyle.Left;
+            MinimumSize = new Size(30, 30);
+            Text = Enum.GetName(keyCode.GetType(), keyCode);
+            ReadOnly = true;
+            Size = new Size(30, 30);
+            TextAlign = HorizontalAlignment.Center;
+
+            Unsubscribe = Observable.FromEventPattern(ev => KeyEvent += ev, ev => KeyEvent -= ev)
+                .Do(SetColor(Color.Red))
+                .Throttle(TimeSpan.FromMilliseconds(1000))
+                .Do(SetColor(Color.Yellow)).Subscribe();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Unsubscribe.Dispose();
+            base.Dispose(disposing);
+        }
+
+
+        private Action<EventPattern<object>> SetColor(Color color)
+        {
+            return (_) =>
+            {
+                BackColor = color;
+            };
+        }
+
+        public void Trigger()
+        {
+            KeyEvent(null, EventArgs.Empty);
+        }
     }
 
     public class KeyGrid : FlowLayoutPanel

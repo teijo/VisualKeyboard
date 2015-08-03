f29ae5d4460792495cc86143661120857472293b
Teijo Laine <teijo@aropupu.fi>
2015-08-03 23:31:09 +0300

    Remove useless import

--- VisualKeyboard/Program.cs
+++ VisualKeyboard/Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Reactive;
 using System.Reactive.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
